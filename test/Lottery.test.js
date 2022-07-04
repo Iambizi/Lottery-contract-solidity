90a7f555182715e1d7454b2dcf472bfb085905f0
Iambizi <amirbzmn@gmail.com>
2022-07-03 21:44:26 -0400

    Wrote new tests: multilple accounts can enter, ensure we send minimum required funds

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -24,4 +24,64 @@ describe('Lottery contract', () => {
         // contract was deployed at
         assert.ok(lottery.options.address);
     });
+
+    it('allows one account to enter the lottery', async()=>{
+        await lottery.methods.enter().send(
+            {
+                from: accounts[0],
+                value: web3.utils.toWei('0.02', 'ether')
+            }
+        );
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+    });
+
+    it('ensure that multiple accounts can enter', async()=>{
+        await lottery.methods.enter().send(
+            {
+                from: accounts[0],
+                value: web3.utils.toWei('0.02', 'ether')
+            }
+        );
+        await lottery.methods.enter().send(
+            {
+                from: accounts[1],
+                value: web3.utils.toWei('0.02', 'ether')
+            }
+        );
+        await lottery.methods.enter().send(
+            {
+                from: accounts[2],
+                value: web3.utils.toWei('0.02', 'ether')
+            }
+        );
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
+    });
+
+    it('requires a minimum amount of ether to enter', async()=>{
+        //Use try catch statement to make sure we get an error when
+        //sending insufficient funds to enter lottery
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            });
+            assert(false);
+        } catch(err){
+            assert(err);
+        }
+    });
 });
